2ae57fd32763730e94b8d25b548c886a8fe984aa
taemin_Kong <rhdxoals@nexon.co.kr>
2024-02-05 14:38:12 +0900

    :label: additionalHosts optional

--- src/vite-plugin-change-host.ts
+++ src/vite-plugin-change-host.ts
@@ -1,7 +1,7 @@
 import { writeHost } from './host.js';
 import type {Plugin, ResolvedConfig} from 'vite';
 
-type Options = string | { host: string; additionalHosts: string[] };
+type Options = string | { host: string; additionalHosts?: string[] };
 
 function changeHost(options:Options):Plugin {
     let _config:ResolvedConfig;

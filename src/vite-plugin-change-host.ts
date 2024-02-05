6000eb522cd8d85a3ff54032441a192ce6799bb8
taemin_Kong <rhdxoals@nexon.co.kr>
2024-02-05 14:25:02 +0900

    work only dev mode

--- src/vite-plugin-change-host.ts
+++ src/vite-plugin-change-host.ts
@@ -1,9 +1,10 @@
 import { writeHost } from './host.js';
-import type { Plugin } from 'vite';
+import type {Plugin, ResolvedConfig} from 'vite';
 
 type Options = string | { host: string; additionalHosts: string[] };
 
 function changeHost(options:Options):Plugin {
+    let _config:ResolvedConfig;
     const host = typeof options === 'string' ? options : options.host;
     const additionalHosts = typeof options === 'string' ? [] : options.additionalHosts || [];
     return {
@@ -16,7 +17,11 @@ function changeHost(options:Options):Plugin {
                 host,
             },
         }),
+        configResolved(config){
+            _config = config;
+        },
         buildStart() {
+            if(_config.mode !== 'development') return;
             writeHost([host, ...additionalHosts]);
         },
     };
